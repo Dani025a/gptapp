646f25c37cc3cbfc4ea7e62ee2871a24519e2e99
Dani025a <dani025a@edu.easj.dk>
2023-09-07 23:28:00 +0200

    qwe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { CTA, Brand, Navbar } from './componets'
 import './app.css'
+
 const App = () => {
   return (
     <div className='App'>
